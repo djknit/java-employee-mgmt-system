3d8578101e7c7cbf019e670227abd2760c5df8f7
David <djknit@gmail.com>
2021-06-11 01:22:33 -0500

    added methods to add new employee

---
+++ src/com/cognixia/jump/djk/firstjavaproject/inputs/EmployeeAdder.java
@@ -0,0 +1,59 @@
+package com.cognixia.jump.djk.firstjavaproject.inputs;
+
+import com.cognixia.jump.djk.firstjavaproject.Divider;
+import com.cognixia.jump.djk.firstjavaproject.DollarAmountInHandler;
+import com.cognixia.jump.djk.firstjavaproject.Executor;
+import com.cognixia.jump.djk.firstjavaproject.StringInHandler;
+import com.cognixia.jump.djk.firstjavaproject.data.Company;
+import com.cognixia.jump.djk.firstjavaproject.data.Department;
+import com.cognixia.jump.djk.firstjavaproject.data.DollarAmount;
+import com.cognixia.jump.djk.firstjavaproject.data.Employee;
+import com.cognixia.jump.djk.firstjavaproject.data.HumanName;
+import com.cognixia.jump.djk.firstjavaproject.menus.Menus;
+
+public class EmployeeAdder {
+	
+	private static final Executor cancel = Menus::employees;
+	
+	private String firstName;
+	private HumanName name;
+	private DollarAmount salary;
+	private Department department;
+	
+	public void run() {
+		Executor addEmpAndProcede = () -> this.addEmployee(cancel);
+		getNameInput(() -> getSalaryInput(addEmpAndProcede));
+	}
+	
+	private void getNameInput(Executor next) {
+		StringInHandler handleInput = (String firstNameIn) -> {
+			firstName = firstNameIn;
+			getLastNameIn(next);
+		};
+		new TextInput("Enter the employee's first name.", handleInput, cancel).run();
+	}
+	
+	private void getLastNameIn(Executor next) {
+		StringInHandler handleInput = (String lastNameIn) -> {
+			name = new HumanName(firstName, lastNameIn);
+			next.execute();
+		};
+		new TextInput("Enter the employee's last name.", handleInput, cancel).run();
+	}
+	
+	private void getSalaryInput(Executor next) {
+		DollarAmountInHandler handleInput = (DollarAmount salaryInput) -> {
+			salary = salaryInput;
+			next.execute();
+		};
+		new DollarsInput("Enter the employee's salary.", handleInput, cancel).run();
+	}
+	
+	private void addEmployee(Executor next) {
+		Employee newEmployee = new Employee(name, salary, department);
+		Company.addEmployee(newEmployee);
+		System.out.println(Divider.get() + "\nNew employee added:\n" + newEmployee);
+		new AnythingInput(next).run();
+	}
+	
+}
